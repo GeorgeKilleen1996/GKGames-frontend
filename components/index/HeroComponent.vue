52110b1bb99dc624431de3ae6d45c3af2f902655
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-06-08 22:56:27 +0100

    style(index): added basic hero placeholder

--- components/index/HeroComponent.vue
+++ components/index/HeroComponent.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="w-full max-w-[90rem] mx-auto lg:px-5 py-[1.5rem]">
-        <div class="w-full h-[30rem] bg-black rounded-2xl flex flex-col justify-center items-center gap-2">
-            <div class="text-3xl font-semibold tracking-wide text-white">Coming Soon</div>
-            <div class="text-sm tracking-tight text-white">GK Games is currently undergoing a major redevelopment project!</div>
+        <div class="w-full h-[35rem] bg-black rounded-2xl flex justify-center items-center overflow-hidden relative">
+            <img class="absolute top-0 left-0" src="/img/pokemon/scarletandviolet_hero.jpg" alt="">
+            <div class="bg-white/30 backdrop-blur-sm rounded-full w-6 h-6 flex justify-center items-center absolute top-2 right-2">
+                <i class="fas fa-info text-xs"></i>
+            </div>
+            <div class="flex flex-col justify-center items-center gap-2 bg-white/30 backdrop-blur-sm rounded-xl p-8 relative">
+                <div class="text-3xl font-semibold tracking-wide text-white uppercase">Coming Soon</div>
+                <div class="text-sm tracking-tight text-white">GK Games is currently undergoing a major redevelopment project!</div>
+            </div>
         </div>
     </div>
 </template>
