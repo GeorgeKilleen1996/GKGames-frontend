ae29dc121f4f483c54c90082622f6874a21ab571
GeorgeKilleen1996 <80597901+GeorgeKilleen1996@users.noreply.github.com>
2023-06-08 20:13:55 +0100

    style(index): removed unused hero code

--- components/index/HeroComponent.vue
+++ components/index/HeroComponent.vue
@@ -1,20 +1,7 @@
 <script setup>
-const images = [
-    '/hero/1.png',
-]
 </script>
 <template>
     <div class="w-full max-w-[90rem] relative">
         
     </div>
 </template>
-
-<style>
-.hero-right-background-zinc{
-    clip-path: polygon(64.5% 0, 100% 0, 100% 100%, 44.5% 100%);
-}
-
-.hero-right-background-red{
-    clip-path: polygon(64% 0, 100% 0, 100% 100%, 44% 100%);
-}
-</style>
